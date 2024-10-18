e7486c1cb1cb007157fc918d82cc57fe6213ad3a
Waller_Shaun <3022943831@qq.com>
2024-10-18 17:07:20 +0800

    update mysql pool

--- index.js
+++ index.js
@@ -6,19 +6,32 @@ const app = express();
 const port = 3000;
 
 // 创建数据库连接
-const connection = mysql.createConnection({
-    host: '112.124.65.59',      // MySQL服务器地址
-    user: 'fortest',           // MySQL用户名
-    password: 'az6KjRyP5sANCapw', // MySQL密码
-    database: 'fortest'  // 你创建的数据库名
-  });
+const pool = mysql.createPool({
+    host: '112.124.65.59',      
+    user: 'fortest',           
+    password: 'az6KjRyP5sANCapw', 
+    database: 'fortest',
+    waitForConnections :true,
+    connectionLimit:10,
+    connectTimeout: 10000,
+    keepAliveInitialDelay: 10000,
+    queueLimit:0
+}) 
 
-connection.connect((err) => {
-if (err) {
-  console.error('数据库连接失败:', err.stack);
-  return;
-}
-console.log('成功连接到数据库');
+
+pool.getConnection((err,connection) => {
+    if (err) {
+        console.error('无法获取数据库连接', err);
+        return;
+    }
+    connection.query('SELECT * FROM contacts', (err, results) => {
+        connection.release();  // 释放连接回连接池
+        if (err) {
+            console.error('查询出错', err);
+            return;
+        }
+        //console.log(results);
+    });
 });
 
 
@@ -27,7 +40,7 @@ app.use(express.json())//中间键，用于解析json请求
 //读取联系人数据
 const readContacts = (callback) => {
   const query='SELECT * FROM contacts';
-  connection.query(query,(err,results)=>{
+  pool.query(query,(err,results)=>{
     if(err){
         console.error('读取数据库联系人数据失败',err);
         callback(err,null); // 返回错误给调用者
@@ -51,7 +64,7 @@ app.get('/contacts',(req,res)=>{
 app.post('/contacts',(req,res)=>{
     const{name,phone} = req.body;
     const query='INSERT INTO contacts (name, phone) VALUES (?, ?)';
-    connection.query(query,[name,phone],(err,results)=>{
+    pool.query(query,[name,phone],(err,results)=>{
         if(err){
             return res.status(500).json({message:"服务器错误"});
         }
@@ -68,7 +81,7 @@ app.put('/contacts/:id',(req,res)=>{
     const{id}=req.params;
     const{name,phone}=req.body;
     const query='UPDATE contacts SET name = ?, phone = ? WHERE id = ?';
-    connection.query(query,[name,phone,id],(err,results)=>{
+    pool.query(query,[name,phone,id],(err,results)=>{
         if(err){
             console.error('数据更新失败',err)
             return res.status(500).json({message:"服务器错误"});
@@ -86,7 +99,7 @@ app.put('/contacts/:id',(req,res)=>{
 app.delete('/contacts/:id',(req,res)=>{
     const{id} = req.params;
     const query='DELETE FROM contacts WHERE id = ?';
-    connection.query(query,[id],(err,results)=>{
+    pool.query(query,[id],(err,results)=>{
         if(err){
             console.error('数据删除失败',err);
             return res.status(500).json({message:"服务器错误"});
